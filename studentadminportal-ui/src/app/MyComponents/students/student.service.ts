817485add3d70205bae8f61f96518c21ffccf621
saprabh20 <saprabhanand20@gmail.com>
2022-11-04 23:44:21 +0530

    End of Section 7

--- studentadminportal-ui/src/app/MyComponents/students/student.service.ts
+++ studentadminportal-ui/src/app/MyComponents/students/student.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { AddStudentRequest } from 'src/app/models/api-models/add-student-request.model';
 import { Student } from 'src/app/models/api-models/student.model';
 import { UpdateStudentRequest } from 'src/app/models/api-models/update-student-request.model';
 
@@ -39,5 +40,21 @@ export class StudentService {
 
   deleteStudent(studentId: string): Observable<Student> {
     return this.httpClient.delete<Student>(this.baseApiUrl + '/students/' + studentId);
+  }
+
+  addStudent(studentRequest: Student): Observable<Student> {
+    const addStudentRequest: AddStudentRequest={
+      firstName: studentRequest.firstName,
+      lastName: studentRequest.lastName,
+      dateOfBirth: studentRequest.dateOfBirth,
+      email: studentRequest.email,
+      mobile: studentRequest.mobile,
+      genderId: studentRequest.genderId,
+      physicalAddress: studentRequest.address.physicalAddress,
+      postalAddress: studentRequest.address.postalAddress
+    }
+
+    return this.httpClient.post<Student>(this.baseApiUrl + "/students/add",
+    addStudentRequest);
   }
 }
--- studentadminportal-ui/src/app/MyComponents/view-student/view-student.component.ts
+++ studentadminportal-ui/src/app/MyComponents/view-student/view-student.component.ts
@@ -13,6 +13,7 @@ import { StudentService } from '../students/student.service';
 })
 export class ViewStudentComponent implements OnInit {
   studentId: string | null | undefined;
+
   student: Student = {
     id: '',
     firstName: '',
@@ -32,6 +33,11 @@ export class ViewStudentComponent implements OnInit {
       postalAddress: ''
     }
   }
+
+  isNewStudent = false;
+
+  header='';
+
   genderList: Gender[] = [];
 
   constructor(private readonly studentService: StudentService,
@@ -45,12 +51,26 @@ export class ViewStudentComponent implements OnInit {
       (params) => {
         this.studentId = params.get("id");
         if (this.studentId) {
-          this.studentService.getStudent(this.studentId)
-            .subscribe(
-              (successResponse) => {
-                this.student = successResponse;
-              }
-            );
+
+          if(this.studentId.toLocaleLowerCase()==='add'.toLocaleLowerCase()){
+            //new student functionality
+            this.isNewStudent=true;
+            this.header = 'Add New Student';
+
+          }else{
+            //existing student functionality
+            this.isNewStudent=false;
+            this.header = 'Edit Student';
+            this.studentService.getStudent(this.studentId)
+              .subscribe(
+                (successResponse) => {
+                  this.student = successResponse;
+                }
+              );
+
+          }
+
+
           this.genderService.getGenderList()
             .subscribe(
               (successResponse) => {
@@ -69,7 +89,7 @@ export class ViewStudentComponent implements OnInit {
     this.studentService.updateStudent(this.student.id, this.student)
       .subscribe(
         (successResponse)=>{
-          console.log(successResponse);
+          // console.log(successResponse);
           this.snackbar.open("Student updated successfully", undefined, {
             duration: 2000
           });
@@ -101,6 +121,26 @@ export class ViewStudentComponent implements OnInit {
       (errorResponse)=>{
         //log
       }
+    )
+  }
+
+  onAdd(): void{
+    this.studentService.addStudent(this.student)
+    .subscribe(
+      (successResponse)=>{
+        this.snackbar.open("Student Added successfully", undefined, {
+          duration: 2000
+        });
+
+        setTimeout(()=>{
+          this.router.navigateByUrl('students/${successResponse.id}');
+        },2000)
+
+
+      },
+      (errorResponse)=>{
+        //log
+      },
     )
   }
 }
