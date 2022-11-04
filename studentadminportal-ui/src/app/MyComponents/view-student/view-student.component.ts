013cb30b4c48a48695ea33125d51ed952c8ca65e
saprabh20 <saprabhanand20@gmail.com>
2022-11-04 22:38:27 +0530

    End of Section 6.1

--- studentadminportal-ui/src/app/MyComponents/view-student/view-student.component.ts
+++ studentadminportal-ui/src/app/MyComponents/view-student/view-student.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Gender } from 'src/app/models/ui-models/gender.model';
 import { Student } from 'src/app/models/ui-models/student.model';
 import { GenderService } from 'src/app/services/gender.service';
@@ -37,7 +37,8 @@ export class ViewStudentComponent implements OnInit {
   constructor(private readonly studentService: StudentService,
     private readonly route: ActivatedRoute,
     private readonly genderService: GenderService,
-    private snackbar: MatSnackBar) { }
+    private snackbar: MatSnackBar,
+    private router: Router) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(
@@ -85,4 +86,21 @@ export class ViewStudentComponent implements OnInit {
 
   }
 
+  onDelete(): void{
+    this.studentService.deleteStudent(this.student.id)
+    .subscribe(
+      (successResponse)=>{
+        this.snackbar.open('Student Deleted successfully', undefined, {
+          duration:2000
+        });
+
+        setTimeout(()=>{
+          this.router.navigateByUrl("students")
+        },2000)
+      },
+      (errorResponse)=>{
+        //log
+      }
+    )
+  }
 }
